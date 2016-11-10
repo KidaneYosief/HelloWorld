060699a97701773f2f4f2b35ee513bfbf1b1080d
Kidane <Kidane@Kidanes-MacBook.local>
2016-11-09 21:42:54 -0600

    Hellow

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Kidane on 11/6/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        for(int i = 1; i<=10; i++) {
+            System.out.println("Hello world ... "+ i);
+        }
+    }
+}
