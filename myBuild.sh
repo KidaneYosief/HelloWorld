2cc2578093cf783bbdd8f20d9668c89ef28d6a19
KidaneYosief <kidaneokbu@gmail.com>
2017-01-31 22:06:47 -0600

    Update myBuild.sh

--- myBuild.sh
+++ myBuild.sh
@@ -1,2 +1,1 @@
-echo "You built your project using this script!"
-echo "this is my first ever practcice with the Pipeline Jenkins"
+echo "You built your project using this script! this is my first ever practcice with the Pipeline Jenkins"
